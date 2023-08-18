1caf020f93c7b84fc7ce961d2e4a17d7f2012d7f
Péter Kalicz <kaliczp@gmail.com>
2023-08-18 15:05:52 +0200

    Pin changed according to schematics

--- hvlog2.c
+++ hvlog2.c
@@ -5,27 +5,25 @@ void dummy ( unsigned int );
 #define GPIOABASE 0x50000000
 #define RCCBASE   0x40021000
 
-int notmain ( void )
-{
+int notmain ( void ) {
     unsigned int ra;
 
     ra=GET32(RCCBASE+0x2C);
-    ra|=1<<2; //enable port c
+    ra|=1<<0; //enable port c
     PUT32(RCCBASE+0x18,ra);
     //moder
     ra=GET32(GPIOABASE+0x00);
-    ra&=~(3<<16); //PA8
-    ra|=1<<16; //PA8 output
+    ra&=~(3<<0); //PA0
+    ra|=1<<1; //PA0 output
     PUT32(GPIOABASE+0x00,ra);
     //OTYPER
     ra=GET32(GPIOABASE+0x04);
-    ra&=~(1<<8); //PA8
+    ra&=~(1<<0); //PA0
     PUT32(GPIOABASE+0x04,ra);
-    while(1)
-    {
-        PUT32(GPIOABASE+0x18,1<<(8+0));
+    while(1) {
+        PUT32(GPIOABASE+0x18,1<<(0+0));
         for(ra=0;ra<200000;ra++) dummy(ra);
-        PUT32(GPIOABASE+0x18,1<<(8+16));
+        PUT32(GPIOABASE+0x18,1<<(0+16));
         for(ra=0;ra<200000;ra++) dummy(ra);
     }
     return(0);
